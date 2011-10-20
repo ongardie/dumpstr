65078eb40a5779ad632404257824659659d7477c
Diego Ongaro <ongaro@cs.stanford.edu>
2011-10-19 17:52:25 -0700

    Serve static files when running the development server

--- urls.py
+++ urls.py
@@ -1,5 +1,16 @@
 from django.conf.urls.defaults import patterns, include, url
+import settings
 
-urlpatterns = patterns('',
+urlpatterns = []
+
+if settings.DEBUG and hasattr(settings, 'STATIC_DOC_ROOT'):
+    urlpatterns += patterns('',
+        url(settings.STATIC_URL[1:] + r'(?P<path>.*)$',
+            'django.views.static.serve',
+            {'document_root': settings.STATIC_DOC_ROOT,
+             'show_indexes': True})
+    )
+
+urlpatterns += patterns('',
     url('^.*$', 'webmetrics.views.home'),
 )
